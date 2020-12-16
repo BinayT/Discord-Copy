1f2e6edd4b8366198a5ff70d2b6430199342bf38
Binay <binayprogramming@gmail.com>
2020-12-16 22:21:48 +0100

    Redux basic config set, actions and reducers made

--- src/store.js
+++ src/store.js
@@ -1,3 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './reducers/userReducer';
 
-export default configureStore({});
+export default configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
