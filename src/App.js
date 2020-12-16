09e6af435176e5a956c27c94ba191febd7819974
Binay <binayprogramming@gmail.com>
2020-12-16 16:25:27 +0100

    Chat section started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Sidebar from './components/Sidebar/Sidebar';
+import Chat from './components/Chat/Chat';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       {/* SideBar */}
       <Sidebar />
       {/* Chat */}
+      <Chat />
     </div>
   );
 }
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Chat() {
+  return <div className='chat'>Chateroni</div>;
+}
+
+export default Chat;
