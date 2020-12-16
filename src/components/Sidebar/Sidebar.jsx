ce5a2b350fb02f45375bb3f376f669f911064f79
Binay <binayprogramming@gmail.com>
2020-12-16 16:13:31 +0100

    Sidebar layout done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { ExpandMore } from '@material-ui/icons';
+import {
+  ExpandMore,
+  SignalCellularAltOutlined,
+  InfoOutlined,
+  MicNoneOutlined,
+  HeadsetOutlined,
+  SettingsOutlined,
+} from '@material-ui/icons';
 import AddIcon from '@material-ui/icons/Add';
+import PhoneEnabledIcon from '@material-ui/icons/PhoneEnabled';
 
 import './Sidebar.css';
 import SidebarChannel from './SidebarChannel/SidebarChannel';
+import { Avatar } from '@material-ui/core';
 
 function Sidebar() {
   function handleAdd() {}
@@ -30,6 +39,34 @@ function Sidebar() {
           <SidebarChannel />
           <SidebarChannel />
           <SidebarChannel />
+        </div>
+      </div>
+
+      <div className='sidebar__voice'>
+        <SignalCellularAltOutlined
+          className='sidebar__voiceIcon'
+          fontSize='large'
+        />
+        <div className='sidebar__voiceInfo'>
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+        <div className='sidebar__voiceIcons'>
+          <PhoneEnabledIcon />
+          <InfoOutlined />
+        </div>
+      </div>
+
+      <div className='sidebar__profile'>
+        <Avatar />
+        <div className='sidebar__profileInfo'>
+          <h3>Binay Timilsina</h3>
+          <p>@myid</p>
+        </div>
+        <div className='sidebar__profileIcons'>
+          <MicNoneOutlined />
+          <HeadsetOutlined />
+          <SettingsOutlined />
         </div>
       </div>
     </div>
